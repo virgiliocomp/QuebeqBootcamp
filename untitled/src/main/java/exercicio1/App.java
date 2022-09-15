1f5a4922069e6831a90fca0e831b54e41ec543e9
Virgilio Cruvinel <virgilio.cruvinel_ntconsult@prestador.globo>
2022-09-15 16:14:36 -0300

    Exericio 1

---
+++ untitled/src/main/java/exercicio1/App.java
@@ -0,0 +1,11 @@
+package exercicio1;
+import exercicio1.Calculadora;
+import exercicio1.Emprestimo;
+import exercicio1.Mensagem;
+
+
+public class App {
+    public static void main(String[] args) {
+        Mensagem.retornaHoraDia();
+    }
+}
